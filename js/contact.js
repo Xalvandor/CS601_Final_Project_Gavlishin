c441f448cd66ee7c96edebd38eed0b1ce3dec3f4
Xalvandor <nikolaygavlishin@gmail.com>
2025-11-24 20:37:59 -0500

    first commit

---
+++ js/contact.js
@@ -0,0 +1,86 @@
+const myForm = document.forms.myForm;
+myForm.addEventListener("submit", validateForm);
+
+function validateForm() {
+
+  // references to DOM elements
+  // first two are let to allow capitalization of names later on
+  let firstName = document.getElementById("firstname");
+  let lastName = document.getElementById("lastname");
+  const email = document.getElementById("email");
+  const message = document.getElementById("message");
+
+  const firstNameError = document.getElementById("firstname-error");
+  const lastNameError = document.getElementById("lastname-error");
+  const emailError = document.getElementById("email-error");
+  const messageError = document.getElementById("message-error");
+
+  //resets error messages
+  firstNameError.textContent = "";
+  lastNameError.textContent = "";
+  emailError.textContent = "";
+  messageError.textContent = "";
+
+  // sets initial validity to true, flips to false if a validation is failed below
+  let isValid = true;
+
+  // first two if statements check for length and alpha only characters using a helper function
+  // false result indicates invalid
+  if (!testName(firstName.value.trim())) {
+    firstNameError.textContent = "Sorry: Last name must contain two (2) or more alpha characters.";
+    isValid = false;
+  }
+
+  if (!testName(lastName.value.trim())) {
+    lastNameError.textContent = "Sorry: Last name must contain two (2) or more alpha characters.";
+    isValid = false;
+  }
+
+  // checks for email validity using a helper function, false result indicates invalid
+  if (!testEmail(email.value.trim())) {
+    emailError.textContent = "Please enter a valid email."
+    isValid = false;
+  }
+
+  // if the value of options is empty, that indicates no package was chosen.
+  if (message.value === "") {
+    messageError.textContent = "Please write a message.";
+    isValid = false;
+  }
+
+  // If it passes all the checks, the success message below is created and displayed.
+  if (isValid) {
+    firstName = firstName.value.trim().replace(/^./, char => char.toUpperCase());
+    lastName = lastName.value.trim().replace(/^./, char => char.toUpperCase());
+
+    document.getElementById('myForm').style.display = 'none';
+    document.getElementById('success-message').style.visibility = 'visible';
+    const successMessage = document.getElementById('success-message');
+    successMessage.innerHTML = `Thank you, ${firstName} ${lastName}, for sending me a message!`;
+    successMessage.style.display = 'grid';
+    return false;
+  }
+    // The return false above and the return false below are to keep the web form from navigating away
+  // I've read that preventDefault is supposed to do that, but I could not get it to work for me.
+  else {
+    return false;
+  }
+}
+
+function testName(string) {
+  // regex for any character that is not a letter
+  const nameRegex = /[^a-zA-Z]+/g;
+
+  // returns true if the name is larger than 1 character and matches the regex
+  return string.trim().length > 1 && nameRegex.test(string) === false;
+
+}
+
+function testEmail(string) {
+  // regex for email validation
+  // allows for alphanumeric characters, and some special characters like  ., _, %, +, and -
+  // domain must be at least two alphanumeric characters
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  // returns true if the email is larger than 1 character and matches the regex
+  return string.trim().length > 1 && emailRegex.test(string) === true;
+}
---
+++ js/quiz.js
@@ -0,0 +1,120 @@
+// references JSON file for sports teams, should work with internet connection
+const apiURL = "https://xalvandor.github.io/CS601_HW2_Gavlishin/sports.json";
+
+// button for loading teams
+const btn = document.getElementById("btn-load-teams");
+
+// references drop zones
+const goodTeamDZ = document.getElementById("Philadelphia");
+const badTeamDZ = document.getElementById("Boston");
+const dropZones = [goodTeamDZ, badTeamDZ];
+
+
+// loads teams into container
+btn.addEventListener('click', () => {
+  //makes sure there aren't more than the total 8 teams
+  if (document.getElementById("container").hasChildNodes() === false ) {
+    fetchSports();
+  } else {
+    alert("You already have the teams loaded!");
+  }
+
+})
+
+// function for fetching JSON from web server
+function fetchSports() {
+  fetch(apiURL)
+    .then((response) => {
+      return response.json();
+    }).then(data => {
+    processData(data);
+    console.log(data);
+  }).catch((err) => {
+    console.log('rejected', err);
+  });
+}
+
+// function for processing fetched Data and appending it to DOM
+function processData(data) {
+  data.forEach(team => {
+    let imgOrder = Math.random() * 10;
+
+    // creates img element using team logo
+    let teamImg = document.createElement('IMG');
+    teamImg.src = team.logoURL;
+    teamImg.id = team.id;
+
+    // teamImg.style.flexBasis = "50%";
+    //teamImg.style.flexShrink = "2";
+    //teamImg.style.flexWrap = "wrap";
+    teamImg.width = 50;
+    teamImg.height = 50;
+
+    teamImg.class = team.city;
+    teamImg.alt = team.city + " " + team.name + " logo. " + team.chant;
+    // creates caption for img
+    let teamElem = document.createElement('figcaption');
+    teamElem.innerText = team.name;
+    teamElem.id = team.id;
+    teamElem.for = team.id;
+    teamElem.class = team.city;
+
+    // creates figure element for img and caption
+    let teamFigure = document.createElement("figure");
+    teamFigure.id = teamImg.id;
+    teamFigure.draggable = true;
+    teamFigure.className = team.city;
+    // teamFigure.style.display = "flex";
+    //teamFigure.style.flexShrink = "2";
+    //teamFigure.style.flexWrap = "wrap";
+    // teamFigure.style.flexFlow = "column wrap";
+    teamFigure.style.order = imgOrder.toString();
+    //dragstart event for figure elem
+    teamFigure.addEventListener('dragstart', (event) => {
+      event.dataTransfer.setData('text/plain', teamFigure.id)
+      event.target.classList.add('dragging');
+
+    });
+    //drag end event for figure elem
+    teamFigure.addEventListener('dragend', (e) => {
+      e.target.classList.remove('dragging');
+      e.target.classList.remove('dragover');
+    })
+    //adds figure, img, and caption to DOM
+    document.getElementById("container").appendChild(teamFigure);
+    document.getElementById(teamFigure.id).appendChild(teamImg);
+    document.getElementById(teamImg.id).appendChild(teamElem);
+    console.log(team.id, team.city, team.name, team.chant);
+  });
+}
+
+// creates drop events for dropzones
+dropZones.forEach(zone => {
+  const dropZone = document.getElementById(zone.id);
+
+  dropZone.addEventListener('drop', (e) => {
+    e.preventDefault();
+
+    const teamID = e.dataTransfer.getData('text/plain');
+    const team = document.getElementById(teamID);
+    // conditional drops based on class names and ids
+    if (team.className === "Philadelphia" && dropZone.id === "Philadelphia") {
+      dropZone.appendChild(team);
+
+    } else if (team.className === "Boston" && dropZone.id === "Boston") {
+      dropZone.appendChild(team);
+    }
+
+    e.target.classList.remove('dragover');
+  })
+  // event to add dragover event, changes color
+  zone.addEventListener('dragover', (event) => {
+    event.preventDefault(); // Necessary to allow dropping
+    event.currentTarget.classList.add('dragover');
+  });
+  // event for dragleave, removes highlighted color
+  zone.addEventListener('dragleave', (e) => {
+    e.preventDefault();
+    e.currentTarget.classList.remove('dragover');
+  });
+})
