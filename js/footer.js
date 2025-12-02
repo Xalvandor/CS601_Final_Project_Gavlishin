f3770b16da1d5517cf901c873e4e768bf39d1346
Xalvandor <nikolaygavlishin@gmail.com>
2025-12-01 20:49:12 -0500

    header and footer react components

--- js/footer.js
+++
@@ -1,9 +0,0 @@
-const year = new Date().getFullYear()
-
-function Footer() {
-  return (<footer>
-            <small>&copy; {year} Portfolio Website.</small>
-  </footer>);
-}
-
-ReactDOM.render(<Footer/>, document.getElementById('footer'));
---
+++ js/headerfooter.js
@@ -0,0 +1,28 @@
+const year = new Date().getFullYear()
+
+function Headerfooter() {
+  return (<footer>
+            <small>&copy; {year} Portfolio Website - Gavlishin</small>
+  </footer>);
+}
+
+ReactDOM.render(<Headerfooter/>, document.getElementById('footer'));
+
+function Header() {
+  return (<header>
+      <nav>
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="resume.html">Resume</a></li>
+          <li><a href="projects.html">Projects</a></li>
+          <li><a href="quiz.html">Quiz</a></li>
+          <li><a href="music.html">Music</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+ReactDOM.render(<Header/>, document.getElementById('header'));
