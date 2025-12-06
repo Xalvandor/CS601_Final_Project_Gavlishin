6c4b6baec759245a7f715920065e8b7a4b862423
Xalvandor <nikolaygavlishin@gmail.com>
2025-12-06 17:16:55 -0500

    added resume toggle button and changed home page styling

--- js/resume.js
+++ js/resume.js
@@ -5,5 +5,18 @@ const message = document.getElementById("message");
 click.addEventListener("click", () => {
   message.innerHTML = "Thank you for downloading my resume!";
   message.style.color = "Green";
-  message.appendChild("<br>");
 });
+
+function toggleResume() {
+  const resumeButton = document.getElementById("resume-button");
+
+  const resumeDoc = document.getElementById("resume-doc");
+
+  if (window.getComputedStyle(resumeDoc).display === "none") {
+    resumeDoc.style.display = "revert";
+    resumeButton.innerText = "Hide Resume";
+  } else {
+    resumeDoc.style.display = "none";
+    resumeButton.innerText = "View Resume";
+  }
+}
