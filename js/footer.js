49847a2be0ba4d9544da6480532e0fd3b0492756
Xalvandor <nikolaygavlishin@gmail.com>
2025-12-07 09:53:11 -0500

    minor change to footer

--- js/footer.js
+++ js/footer.js
@@ -1,10 +1,7 @@
-const year = new Date().getFullYear()
-
-
 function Footer() {
   return (<footer>
-    <img src="data/kolya-high-resolution-logo.png" alt={"Green plant logo"}/>
-    <p>&copy; {year} Portfolio Website - Gavlishin</p>
+    <img src="data/kolya-high-resolution-logo.png" alt="Kolya's green plant logo"/>
+    <p>&copy; {new Date().getFullYear()} Portfolio Website - Gavlishin</p>
     <a href="https://github.com/Xalvandor" target="_blank"><img
       src="https://pngimg.com/uploads/github/github_PNG53.png" alt="GitHub logo"/></a>
     <a href="https://www.youtube.com/@kolya_kvasolya" target="_blank"><img
